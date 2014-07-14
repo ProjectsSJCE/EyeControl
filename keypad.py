b7af467697e61ead5febaf7c7c5d83cb4bef604a
vasanth <vasanth@pc.(none)>
2014-07-15 03:33:16 +0530

    Added functionality, not dealing with space and return

--- keypad.py
+++ keypad.py
@@ -1,10 +1,12 @@
 import wx
+from pykeyboard import PyKeyboard
 
 class MyFrame(wx.Frame):
     
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, 'Keypad', pos=(0, 0), size=wx.DisplaySize())
         self.panel = wx.Panel(self)
+        self.keyboard_control = PyKeyboard()
         
         #place to store what is being entered
         self.text = wx.TextCtrl(self.panel, 10, "Data", size=wx.DLG_SZE(self, 500, 20))
@@ -18,12 +20,15 @@ class MyFrame(wx.Frame):
         self.keys.append('enter')
         
         i = 1
+        self.hash = {}
         self.key_buttons = []
         for key in self.keys:
             b = wx.Button(self.panel, i, key, size=wx.DLG_SZE(self, 71, 70))
             self.key_buttons.append(b)
+            self.Bind(wx.EVT_BUTTON, self.connect_keys, b)
+            self.hash[i] = key
             i += 1
-            
+         
         i = 0
         j = 0
         self.keybox = []
@@ -44,11 +49,21 @@ class MyFrame(wx.Frame):
         self.outer_box.Add(self.keyboard, border=5, flag=wx.ALL)    
         self.panel.SetSizer(self.outer_box)    
         
-#    def connect_keys(self):
+    def connect_keys(self, event):
         
-#    def update_text(self, letter):
+        i = event.GetId()
+        label = self.hash[i]
+#        if label == "space":
+#            label = " "
+#        else:
+#            label = "\n"
+        self.keyboard_control.type_string(label)
+        self.update_text(label)
         
-           
+    def update_text(self, letter):
+        
+        initial_text = self.text.GetValue()
+        self.text.SetValue(initial_text + letter)
         
 class MyApp (wx.App) :
     
